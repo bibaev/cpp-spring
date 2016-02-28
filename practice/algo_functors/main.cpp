32694ec072d7becfd0a4aa94c3689d12ff8d13f9
Roenke <roenke54@gmail.com>
2016-02-28 11:50:36 +0300

    Add sorting tasks. Add inputted filter task.

--- practice/algo_functors/main.cpp
+++ practice/algo_functors/main.cpp
@@ -12,21 +12,31 @@ using cvt = typename container::value_type;
 
 template<typename fwd_it, typename cmp>
 size_t elem_num_in_sorted(fwd_it begin, fwd_it end, 
-        typename std::iterator_traits<fwd_it>::value_type value, 
-        cmp comparer = std::less<typename std::iterator_traits<fwd_it>::value_type>()) {
+        ivt<fwd_it> value, 
+        cmp comparer = std::less<ivt<fwd_it>>()) {
     auto it_lower = std::lower_bound(begin, end, value, comparer);
     auto it_upper = std::upper_bound(begin, end, value, comparer);
     return std::distance(it_lower, it_upper);
 }
 
-template<typename container, typename cmp = std::less<typename container::value_type>>
-typename container::iterator set_add(container & set, typename container::value_type value, cmp comparer = cmp()) {
+template<typename container, typename cmp = std::less<cvt<container>>>
+typename container::iterator set_add(container & set, cvt<container> value, cmp comparer = cmp()) {
     auto pos = std::lower_bound(set.begin(), set.end(), value, comparer);
     if (pos != set.end() && *pos != value) {
         return set.insert(pos, value);
     }
 
     return set.end();
+}
+
+template<typename it>
+void print(it begin, it end) {
+    while(begin != end) {
+        std::cout << *begin << " ";
+        ++begin;
+    }
+
+    std::cout << std::endl;
 }
 
 template<typename container, typename pred>
@@ -37,28 +47,40 @@ void erase_if(container & set, pred predicate) {
 
 template<typename it>
 void merge_sort(it begin, it end) {
-    
+    auto distance = std::distance(begin, end);
+    if(distance > 1) {
+        auto middle = begin + distance / 2;
+        merge_sort(begin, middle);
+        merge_sort(middle, end);
+        std::inplace_merge(begin, middle, end);
+    }
 }
 
 template<typename it>
 void heap_sort(it begin, it end) {
     std::make_heap(begin, end);
+    while(begin != end) {
+        std::pop_heap(begin, end);
+        --end;
+    }
 }
 template<typename Arg>
-struct inputed_filter : std::unary_function<Arg, bool> {
-    Res operator()(Arg const & arg) {
-        if (values_.size() != 5) {
+struct inputed_filter {
+    bool operator()(Arg const & arg) {
+        if (values_.size() < 5) {
             std::cout << "Enter 5 values:" << std::endl;
             while(values_.size() != 5) {
-                
+                Arg value;
+                std::cin >> value;
+                values_.emplace_back(std::move(value));
             }
-            return true;
         }
-        return {};
+
+        return std::find(values_.begin(), values_.end(), arg) != values_.end();
     }
 
 private:
-    std::vector<int> values_;
+    std::vector<Arg> values_;
 };
 
 void tests() {
@@ -78,6 +100,24 @@ void tests() {
     erase_if(vect2, [](int val) { return val > 3; });
     assert(vect2.size() == 3);
     assert(vect2[2] == 3);
+
+    std::vector<int> for_sorting1 = { 0, 4, -2, 5, 6, 1, 3, 10, 22, -40, -50, -30, 23 };
+    std::vector<int> for_sorting2 = { 0, 4, -2, 5, 6, 1, 3, 10, 22, -40, -50, -30, 23 };
+
+    merge_sort(for_sorting1.begin(), for_sorting1.end());
+    heap_sort(for_sorting2.begin(), for_sorting2.end());
+    for (size_t i = 1; i < for_sorting1.size(); ++i) {
+        assert(for_sorting1[i - 1] <= for_sorting1[i]);
+        assert(for_sorting2[i - 1] <= for_sorting2[i]);
+        assert(for_sorting1[i] == for_sorting2[i]);
+        assert(for_sorting1[i - 1] == for_sorting2[i - 1]);
+    }
+
+    std::vector<int> str_vec = { 1, 2, 3, 4, 6 };
+    inputed_filter<int> filter;
+    erase_if(str_vec, std::ref(filter));
+    
+    print(str_vec.begin(), str_vec.end());
 }
 
 int main() {
